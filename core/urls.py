ad4ce3a18381c048ef13f89baab4ded5f8495951
Fernando Souza <fernandovbsouza@gmail.com>
2018-02-14 19:59:10 -0300

    first commit

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from simplemooc.core.views import home, contact
+
+app_name = "core"
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('contato/', contact, name="contato"),
+]
