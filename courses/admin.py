ffaf4b6e92b1b94790cfd02952a5b49f35be1988
Fernando Souza <fernandovbsouza@gmail.com>
2018-02-20 06:03:10 -0300

    added prepopulated fields on courses admin

--- courses/admin.py
+++ courses/admin.py
@@ -5,6 +5,6 @@ class CourseAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'slug', 'start_date', 'created_at']
     search_fields = ['name', 'slug', 'created_at']
-
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Course, CourseAdmin)
