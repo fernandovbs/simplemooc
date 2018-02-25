3afb7e3fa53efcc5d0391b0213dd3159b373461c
Fernando Souza <fernandostorm1@gmail.com>
2018-02-25 19:11:41 -0300

    Added simgle course page

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from simplemooc.courses.views import index
+from simplemooc.courses.views import index, detalhes
 
 app_name = "courses"
 
 urlpatterns = [
     path('', index, name="index"),
+    path('detalhes/<int:pk>/', detalhes, name="detalhes")
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 # Create your views here.
@@ -10,4 +10,14 @@ def index(request):
     }
 
     template_name = 'courses/index.html'
+    return render(request, template_name, context)
+
+def detalhes(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+
+    context = {
+        'course': course,
+    }
+
+    template_name = 'courses/details.html'
     return render(request, template_name, context)
