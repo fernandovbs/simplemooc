d52f24f6cce7bf6980138e1a4975037c3f573060
Fernando Souza <fernandostorm1@gmail.com>
2018-02-28 06:45:05 -0300

    Added contact form in courses details page

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 # Create your views here.
 def index(request):
@@ -14,9 +15,15 @@ def index(request):
 
 def detalhes(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+    else:
+        form = ContactCourse()
 
     context = {
         'course': course,
+        'form': form
     }
 
     template_name = 'courses/details.html'
