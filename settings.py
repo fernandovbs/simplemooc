5bf9c679f11906eafa6e1be272408cb42157347f
Fernando Souza <fernandostorm1@gmail.com>
2018-03-18 07:36:03 -0300

    Added frontend login rote

--- settings.py
+++ settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'simplemooc.core',
     'simplemooc.courses',
+    'simplemooc.accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,15 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
+
+#E-mails
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'fernandostorm1@gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'fernandostorm1@gmail.com'
+EMAIL_HOST_PASSWORD = '@Aliados12' 
+EMAIL_PORT = 587
+
+CONTACT_EMAIL = 'fernandovbsouza@gmail.com'
